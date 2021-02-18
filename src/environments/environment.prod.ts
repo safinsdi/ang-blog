7370e0d7506ae7baaad2e18b966230a4ece76c34
safina_di <safidilya@gmail.com>
2021-02-19 02:31:40 +0500

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAhpvUgTSKZb-tT2BpOkL0fy6NK1jnINZs',
+  fbDbUrl: 'https://blog-e8023-default-rtdb.firebaseio.com'
+};
